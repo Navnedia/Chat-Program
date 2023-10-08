b84cc5f4ebdfcd35fa69bee00e1c8ada070b49e1
Navnedia <87249556+Navnedia@users.noreply.github.com>
2023-10-08 03:01:27 -0500

    Modified messenger to become chat client
    Removed the server-side code and added prompt for username.

--- ChatClient.java
+++ ChatClient.java
@@ -1,58 +1,52 @@
-import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
- * A simple messenger application for simple two-way communication over a {@link Socket}
- * connection.<br>
- *
- * Usage:<br>
- *      - javac Messenger.java<br>
- *      - java Messenger [-l] <port> [host]
+ * A simple chat client application for connecting to a {@link ChatServer}
+ * over a {@link Socket} connection.
+ * <br><br>
+ * <strong>Program Usage:</strong>
+ * <ol>
+ *     <li>Compile the code using:
+ *          <strong>{@code javac ChatClient.java}</strong>.
+ *     </li>
+ *     <li>Run the client with the port and host of the server:
+ *          <strong>{@code java ChatClient <port> [host]}</strong>.
+ *     </li>
+ *     <li>Enter a client username when prompted.</li>
+ *     <li>Now you can start chatting.</li>
+ * </ol>
  *
  * @author Aiden Vandekerckhove
  */
-public class Messenger {
+public class ChatClient {
 
     public static void main(String[] args) {
-        boolean listenServer = false;
         String host = "localhost";
         int port = 0;
 
         // Parse command line arguments to extract run configuration:
         try {
-            if (args.length < 1) {
-                printUsage();
-            } else if (args[0].equals("-l")) {
-                listenServer = true;
-                if (args.length >= 2) {
-                    port = Integer.parseInt(args[1]);
-                    if (args.length == 3) {
-                        host = args[2];
-                    }
-                } else {
-                    printUsage();
-                }
-            } else {
-                port = Integer.parseInt(args[0]);
-                if (args.length == 2) {
-                    host = args[1];
-                }
+            if (args.length < 1 || args.length > 2) { printUsage(); }
+
+            port = Integer.parseInt(args[0]);
+            if (args.length == 2) {
+                host = args[1];
             }
         } catch (NumberFormatException e) {
             printUsage();
         }
 
-        // Start sockets and run messenger functionality:
+        // Create socket connection to the server and run threads for simultaneous sending/receiving:
+        // start the chat functionality.
         try {
-            Socket clientSocket;
-            if (listenServer) {
-                // Create server socket, bind to port, start listening, then get client socket from accepted connection:
-                ServerSocket serverSocket = new ServerSocket(port);
-                clientSocket = serverSocket.accept();
-                serverSocket.close();
-            } else { // Client application.
-                clientSocket = new Socket(host, port);
-            }
+            System.out.println("Waiting for request from server...");
+            Socket clientSocket = new Socket(host, port); // Create socket and connect to server on the specified host/port.
+
+            setUsername(clientSocket); // Get username and sent it to the server.
 
             // Start the socket connection workers to send and receive data from the socket connection:
             // The sender runs on a separate Thread  to allow the application to send and receive at the same time.
@@ -69,9 +63,31 @@ public class Messenger {
         }
     }
 
+    /**
+     * Prompt the user to input a username and send the selected username to the server over the
+     * {@link Socket} connection.
+     *
+     * @param clientSocket the active {@link Socket} connection object.
+     * @throws IOException if an I/O exception occurs.
+     */
+    public static void setUsername(Socket clientSocket) throws IOException  {
+        System.out.println("What is your name?"); // Prompt client to give a username.
+        // Buffer to read in Standard input from user line-by-line.
+        BufferedReader stdinBuffer = new BufferedReader(new InputStreamReader(System.in));
+        // Output stream for sending data over the socket connection.
+        DataOutputStream socketOut = new DataOutputStream(clientSocket.getOutputStream());
+
+        String message;
+        // Read in username from standard input and forward to the server:
+        if ((message = stdinBuffer.readLine()) != null) {
+            socketOut.writeUTF(message);
+            System.out.println("Sending name to server...");
+        }
+    }
+
     public static void printUsage() {
         System.out.println("\nInvalid or missing arguments!\nUsage:" +
-                "\n\tjava Messenger [-l] <port> [host]\n");
+                "\n\tjava ChatClient <port> [host]\n");
         System.exit(0);
     }
 }
